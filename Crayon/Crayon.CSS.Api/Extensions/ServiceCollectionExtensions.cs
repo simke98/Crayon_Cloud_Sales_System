757d1fc6706e5a1ab5fffe833715cfed3285cf87
Aca Simic <simicaca98@gmail.com>
2025-02-20 12:26:27 +0100

    add swagger

---
+++ Crayon/Crayon.CSS.Api/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.OpenApi.Models;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Crayon.CSS.Api.Extensions;
+
+public static class ServiceCollectionExtensions
+{
+    public static void AddSwaggerSettings(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Cloud Sales System", Version = "v1" });
+        });
+    }
+}
--- Crayon/Crayon.CSS.Api/Startup.cs
+++ Crayon/Crayon.CSS.Api/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Crayon.CSS.Persistence;
+﻿using Crayon.CSS.Api.Extensions;
+using Crayon.CSS.Persistence;
 using Microsoft.EntityFrameworkCore;
 
 namespace Crayon.CSS.Api;
@@ -18,6 +19,7 @@ public class Startup
         services.AddDbContext<CSSDBContext>(opts =>
             opts.UseSqlServer(Configuration.GetConnectionString("CLOUD_SALES_SYSTEM_DB")));
 
+        services.AddSwaggerSettings();
         services.AddHttpClient();
         services.AddCors(options =>
         {
@@ -53,6 +55,14 @@ public class Startup
         app.UseEndpoints(endpoints =>
         {
             endpoints.MapControllers();
+        });
+
+
+        app.UseSwagger();
+        app.UseSwaggerUI(c =>
+        {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "Cloud Sales System API v1");
+            c.DocumentTitle = "Cloud Sales System Swagger";
         });
 
     }
